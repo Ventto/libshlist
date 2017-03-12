830062584fa472ca955743189f7f0ebeabf04f26
Thomas Venries <thomas.venries@gmail.com>
2017-03-13 00:33:51 +0100

    src: Added tests.

---
+++ src/test.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+. ./array.sh
+
+array_test () {
+    arr="$1"
+
+    echo -n 'Length of array: '
+    echo "$(array_len "$arr")"
+
+    echo -n 'Get idx=1: '
+    echo "$(array_get 1 "$arr")"
+
+    echo -n 'Get idx=2: '
+    echo "$(array_get 2 "$arr")"
+
+    echo -n 'Get idx of "e4": '
+    echo "$(array_indexof 'e4' "$arr")"
+
+    echo -n 'Get idx of "e2": '
+    echo "$(array_indexof 'e2' "$arr")"
+
+    echo -n 'Get idx of "foo": '
+    echo "$(array_indexof 'foo' "$arr")"
+
+    echo 'Delete "e3": '
+    arr="$(array_del 'e3' "$arr")"
+    echo "$arr"
+
+    echo 'Delete "index 1": '
+    arr="$(array_delindex '1' "$arr")"
+    echo "$arr"
+
+    echo 'Add "e5": '
+    arr="$(array_add 'e5' "$arr")"
+    echo "$arr"
+
+    echo -n 'Contains "e3" ?: '
+    if array_contains 'e3' "$arr"; then
+        echo 'yes'
+    else
+        echo 'no'
+    fi
+
+    echo -n 'Contains "e5" ?: '
+    if array_contains 'e5' "$arr"; then
+        echo 'yes'
+    else
+        echo 'no'
+    fi
+}
+
+echo "\n==== Not Empty ====\n"
+arr="$(array "e1 e2 e3 e4")"
+array_test "$arr"
+
+echo "\n==== Empty ====\n"
+arr=
+array_test "$arr"
+
+
