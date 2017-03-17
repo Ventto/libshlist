5c442f9f5431f79f249a07c8bfb4ad1838c3cc92
Thomas Venries <thomas.venries@gmail.com>
2017-03-17 20:59:36 +0100

    test: Updated tests.

--- test.sh
+++ test.sh
@@ -4,79 +4,49 @@
 
 list_test () {
     lst="$1"
+    printf "Prints lst:%s\n\n" "$lst"
+    printf "Length of lst: %s\n" "$(list_len "$lst")"
+    printf "lst[1]: %s\n" "$(list_get 1 "$lst")"
+    printf "lst[2]: %s\n" "$(list_get 2 "$lst")"
+    printf "Index of 'e4': %s\n" "$(list_indexof 'e4' "$lst")"
+    printf "Index of 'e2': %s\n" "$(list_indexof 'e2' "$lst")"
+    printf "Index of 'foo': %s\n" "$(list_indexof 'foo' "$lst")"
 
-    echo -n 'Length of list: '
-    echo "$(list_len "$lst")"
-
-    echo -n 'Get idx=1: '
-    echo "$(list_get 1 "$lst")"
-
-    echo -n 'Get idx=2: '
-    echo "$(list_get 2 "$lst")"
-
-    echo -n 'Get idx of "e4": '
-    echo "$(list_indexof 'e4' "$lst")"
-
-    echo -n 'Get idx of "e2": '
-    echo "$(list_indexof 'e2' "$lst")"
-
-    echo -n 'Get idx of "foo": '
-    echo "$(list_indexof 'foo' "$lst")"
-
-    echo 'Delete "e3": '
     lst="$(list_del 'e3' "$lst")"
-    echo "$lst"
+    printf "Delete 'e3':\n%s\n\n" "$lst"
 
-    echo 'Delete "index 2": '
     lst="$(list_delindex '2' "$lst")"
-    echo "$lst"
+    printf "Delete lst[2]:\n%s\n\n" "$lst"
 
-    echo 'Add "e5": '
     lst="$(list_add 'e5' "$lst")"
-    echo "$lst"
+    printf "Add 'e5':\n%s\n\n" "$lst"
 
-    echo -n 'Contains "e3" ?: '
-    if list_contains 'e3' "$lst"; then
-        echo 'yes'
-    else
-        echo 'no'
-    fi
+    list_contains 'e5' "$lst" && resp='yes' || resp='no'
+    printf "Contains 'e5' ? %s\n" "$resp"
 
-    echo -n 'Contains "e5" ?: '
-    if list_contains 'e5' "$lst"; then
-        echo 'yes'
-    else
-        echo 'no'
-    fi
+    list_contains 'e3' "$lst" && resp='yes' || resp='no'
+    printf "Contains 'e3' ? %s\n" "$resp"
 
-    echo 'Add "e2": '
+    printf "Head of list:%s\n" "$(list_head "$lst")"
+    printf "Last of list:%s\n" "$(list_last "$lst")"
+
     lst="$(list_add 'e2' "$lst")"
-    echo "$lst"
+    printf "Add 'e2':\n%s\n" "$lst"
+    printf "How many 'e2':\n%s\n" "$(list_occr 'e2' "$lst")"
 
-    echo -n 'How many "e2": '
-    echo "$(list_occr 'e2' "$lst")"
-
-    echo 'Delete "e2": '
     lst="$(list_del 'e2' "$lst")"
-    echo "$lst"
+    printf "Delete 'e2':\n%s\n\n" "$lst"
 
-    echo 'Add 2 x "e2": '
-    lst="$(list_add 'e2' "$lst")"
-    lst="$(list_add 'e2' "$lst")"
-    echo "$lst"
+    lst="$(list_insert 'e2' '0' "$lst")"
+    printf "Insert 'e2' at [0]:\n%s\n\n" "$lst"
+    lst="$(list_insert 'e2' '2' "$lst")"
+    printf "Insert 'e2' at [2]:\n%s\n\n" "$lst"
+    printf "How many 'e2': %s\n" "$(list_occr 'e2' "$lst")"
 
-    echo -n 'How many "e2": '
-    echo "$(list_occr 'e2' "$lst")"
-
-    echo 'Delete all "e2": '
     lst="$(list_delall 'e2' "$lst")"
-    echo "$lst"
-
-    echo -n 'How many "e2": '
-    echo "$(list_occr 'e2' "$lst")"
-
-    echo 'Map::<echo>: '
-    echo "$(list_map 'echo' "$lst")"
+    printf "Delete all 'e2':\n%s\n\n" "$lst"
+    printf "How many 'e2': %s\n" "$(list_occr 'e2' "$lst")"
+    printf "Map::<echo>:\n%s\n" "$(list_map 'echo' "$lst")"
 }
 
 echo "\n==== Not Empty ====\n"
