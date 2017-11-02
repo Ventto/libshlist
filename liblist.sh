2aab682a58f542a0a732c0ad4e5f28e1653e7202
Thomas Venriès <thomas.venries@gmail.com>
2017-11-02 15:16:15 +0100

    misc: Update copyright
    Signed-off-by: Thomas Venriès <thomas.venries@gmail.com>

--- liblist.sh
+++ liblist.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # The MIT License (MIT)
 #
-# Copyright (c) 2015-2016 Thomas "Ventto" Venriès <thomas.venries@gmail.com>
+# Copyright (c) 2017-2018 Thomas "Ventto" Venriès <thomas.venries@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (the "Software"), to deal in
